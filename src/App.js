cce0521d49ef4442bdb4a186d39f16bed8a17694
Lars Risdal <larsrisdal@gmail.com>
2021-08-12 08:34:12 +0200

    stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import Todo from "./components/Todo";
-import { memo } from "React";
+import { Route, Switch } from "react-router-dom";
 
-const App = memo((props) => {
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
+function App() {
   return (
     <div>
-      <div>
-        <h1>My Todos</h1>
-        <Todo text="Learn React"/>
-        <Todo text="Master React"/>
-      </div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
-});
+}
 
 export default App;
