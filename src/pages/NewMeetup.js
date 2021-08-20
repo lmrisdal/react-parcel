8991e62cd330ea5958b01c80670a475d6ba611f1
Lars Risdal <larsrisdal@gmail.com>
2021-08-20 14:29:15 +0200

    add func

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,15 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+
+  function addMeetupHandler(meetupData) {
+    console.log(meetupData);
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>
   );
 }
